c4be518a2f48b3f56d7bd5245db67db3c5bc10bb
Ben Christel <bchristel@pivotal.io>
2021-08-01 09:36:14 -0700

    Add margin adjustment and hanging indents
    This is to support kludgey nested lists. The behavior of actual list
    elements gets confusing when you have lists with multiple paragraphs,
    and nested lists. It's hard to get the editor to do what you want in
    some edge cases, like adding a new paragraph at the beginning of an
    existing list item. For the purposes of Path OS, I don't actually care
    about semantic markup. I'd rather have more direct control over the
    document's appearance.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import {EditorView} from "prosemirror-view"
 import {EditorState} from "prosemirror-state"
 import {DOMParser, Schema} from "prosemirror-model"
-import {schema as baseSchema} from "prosemirror-schema-basic"
 import {baseKeymap} from "prosemirror-commands"
 import {keymap} from "prosemirror-keymap"
 import {exampleSetup, buildMenuItems} from "prosemirror-example-setup"
@@ -16,6 +15,151 @@ import {
   tableNodes as configureTableNodes, fixTables,
 } from "prosemirror-tables"
 
+
+// SCHEMA ==================================================
+
+
+const pDOM          = ["p", 0]
+const blockquoteDOM = ["blockquote", 0]
+const hrDOM         = ["hr"]
+const preDOM        = ["pre", ["code", 0]]
+const brDOM         = ["br"]
+const leftDOM       = ["left", 0]
+const rightDOM      = ["right", 0]
+const centerDOM     = ["center", 0]
+const emDOM         = ["em", 0]
+const strongDOM     = ["strong", 0]
+const codeDOM       = ["code", 0]
+
+const nodes = {
+  doc: {
+    content: "block+"
+  },
+
+  paragraph: {
+    group: "block",
+    content: "inline*",
+    attrs: {
+      indent: {default: "none"},
+      margin: {default: "0"},
+    },
+    parseDOM: [
+      {
+        tag: "p",
+        getAttrs(dom) {
+          return {
+            indent: dom.getAttribute("indent") || "none",
+            margin: dom.getAttribute("margin") || "0"
+          }
+        },
+      },
+    ],
+    toDOM({attrs: {indent, margin}}) { return ["p", {indent, margin}, 0] }
+  },
+
+  blockquote: {
+    content: "block+",
+    group: "block",
+    defining: true,
+    parseDOM: [{tag: "blockquote"}],
+    toDOM() { return blockquoteDOM }
+  },
+
+  horizontal_rule: {
+    group: "block",
+    parseDOM: [{tag: "hr"}],
+    toDOM() { return hrDOM }
+  },
+
+  heading: {
+    group: "block",
+    attrs: {level: {default: 1}},
+    content: "inline*",
+    defining: true,
+    parseDOM: [
+      {tag: "h1", attrs: {level: 1}},
+      {tag: "h2", attrs: {level: 2}},
+      {tag: "h3", attrs: {level: 3}},
+      {tag: "h4", attrs: {level: 4}},
+      {tag: "h5", attrs: {level: 5}},
+      {tag: "h6", attrs: {level: 6}},
+    ],
+    toDOM(node) { return ["h" + node.attrs.level, 0] }
+  },
+
+  code_block: {
+    content: "text*",
+    marks: "",
+    group: "block",
+    code: true,
+    defining: true,
+    parseDOM: [{tag: "pre", preserveWhitespace: "full"}],
+    toDOM() { return preDOM }
+  },
+
+  text: {
+    group: "inline"
+  },
+
+  image: {
+    inline: true,
+    attrs: {
+      src: {},
+      alt: {default: null},
+      title: {default: null}
+    },
+    group: "inline",
+    draggable: true,
+    parseDOM: [{tag: "img[src]", getAttrs(dom) {
+      return {
+        src: dom.getAttribute("src"),
+        title: dom.getAttribute("title"),
+        alt: dom.getAttribute("alt")
+      }
+    }}],
+    toDOM(node) { let {src, alt, title} = node.attrs; return ["img", {src, alt, title}] }
+  },
+
+  hard_break: {
+    inline: true,
+    group: "inline",
+    selectable: false,
+    parseDOM: [{tag: "br"}],
+    toDOM() { return brDOM }
+  }
+}
+
+const marks = {
+  link: {
+    attrs: {
+      href: {},
+      title: {default: null}
+    },
+    inclusive: false,
+    parseDOM: [{tag: "a[href]", getAttrs(dom) {
+      return {href: dom.getAttribute("href"), title: dom.getAttribute("title")}
+    }}],
+    toDOM(node) { let {href, title} = node.attrs; return ["a", {href, title}, 0] }
+  },
+
+  em: {
+    parseDOM: [{tag: "i"}, {tag: "em"}, {style: "font-style=italic"}],
+    toDOM() { return emDOM }
+  },
+
+  strong: {
+    parseDOM: [{tag: "strong"},
+               {tag: "b", getAttrs: node => node.style.fontWeight != "normal" && null},
+               {style: "font-weight", getAttrs: value => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null}],
+    toDOM() { return strongDOM }
+  },
+
+  code: {
+    parseDOM: [{tag: "code"}],
+    toDOM() { return codeDOM }
+  }
+}
+
 const tableNodes = configureTableNodes({
   tableGroup: "block",
   cellContent: "paragraph",
@@ -28,14 +172,34 @@ const tableNodes = configureTableNodes({
   }
 })
 
-console.log(baseSchema.spec.nodes)
-
 const schema = new Schema({
-  nodes: baseSchema.spec.nodes
-    .append(tableNodes)
-    .remove("blockquote"),
-  marks: baseSchema.spec.marks,
+  nodes: {...nodes, ...tableNodes},
+  marks,
 })
+
+// COMMANDS ================================================
+
+function transformAttrs(nodeType, getNewAttrs) {
+  return function(state, dispatch) {
+    let {from, to} = state.selection
+    let applicable = false
+    let newAttrs = {}
+    state.doc.nodesBetween(from, to, (node, pos) => {
+      if (applicable) return false
+      if (!node.isTextblock) return;
+      newAttrs = getNewAttrs(node.attrs)
+      if (node.type != nodeType) return;
+      if (node.hasMarkup(nodeType, newAttrs)) return;
+      applicable = true
+    })
+    if (!applicable) return false
+    if (dispatch) dispatch(state.tr.setBlockType(from, to, nodeType, newAttrs).scrollIntoView())
+    return true
+  }
+}
+
+// MENU ====================================================
+
 
 function item(label, cmd) {
   return new MenuItem({
@@ -46,8 +210,33 @@ function item(label, cmd) {
   })
 }
 
+function indentCmd(indentType) {
+  return transformAttrs(
+    schema.nodes.paragraph,
+    ({indent, ...rest}) => ({indent: indentType, ...rest}),
+  )
+}
+
+function marginCmd(increment) {
+  return transformAttrs(
+    schema.nodes.paragraph,
+    ({margin, ...rest}) => ({
+      margin: String(clamp(+margin + increment, 0, 10)),
+      ...rest,
+    }),
+  )
+}
+
 const menu = [
   ...buildMenuItems(schema).fullMenu,
+  [
+    new Dropdown([
+      item("No Indent", indentCmd("none")),
+      item("Hanging Indent", indentCmd("hanging")),
+      item("Increase Margin", marginCmd(1)),
+      item("Decrease Margin", marginCmd(-1)),
+    ], {label: "Indent"}),
+  ],
   [
     new Dropdown([
       item("Add Table", createTable),
@@ -64,6 +253,8 @@ const menu = [
   ]
 ]
 
+// PLUGINS =================================================
+
 const plugins = [
   tableEditing(),
   keymap({
@@ -73,6 +264,16 @@ const plugins = [
   }),
   ...exampleSetup({schema, menuContent: menu})
 ]
+
+// UTILS ===================================================
+
+function clamp(x, min, max) {
+  if (x < min) return min
+  if (x > max) return max
+  return x
+}
+
+// MAIN ====================================================
 
 const doc = DOMParser.fromSchema(schema).parse(document.querySelector("#content"))
 const state = EditorState.create({doc, plugins: plugins})
